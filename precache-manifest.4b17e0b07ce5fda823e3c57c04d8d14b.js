91d22628f4e7ed8351dd993bbdf3404017649686
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-09-10 13:17:02 +0700

    Updates

---
+++ precache-manifest.4b17e0b07ce5fda823e3c57c04d8d14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de493c21e512dcfa5c145a125e1f8fa",
+    "url": "/recipeapp-react_js-web/index.html"
+  },
+  {
+    "revision": "3ad565e13c01db81c09f",
+    "url": "/recipeapp-react_js-web/static/css/main.491adfe1.chunk.css"
+  },
+  {
+    "revision": "f8f33ec1ad5576be2514",
+    "url": "/recipeapp-react_js-web/static/js/2.cc08dd82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeapp-react_js-web/static/js/2.cc08dd82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad565e13c01db81c09f",
+    "url": "/recipeapp-react_js-web/static/js/main.87f36d33.chunk.js"
+  },
+  {
+    "revision": "8aed0b58123cea48c1dc",
+    "url": "/recipeapp-react_js-web/static/js/runtime-main.f798e6e7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/recipeapp-react_js-web/precache-manifest.4b17e0b07ce5fda823e3c57c04d8d14b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipeapp-react_js-web/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
